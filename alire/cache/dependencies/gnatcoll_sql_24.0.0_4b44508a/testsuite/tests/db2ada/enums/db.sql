a7820716b0f36696c1e789c0b36cd2f1153e6d92
DosKobold <hanspeter56@t-online.de>
2025-02-24 20:06:16 +0100

    ops: remove dependencies

--- alire/cache/dependencies/gnatcoll_sql_24.0.0_4b44508a/testsuite/tests/db2ada/enums/db.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE colors (name text, value int);
-INSERT INTO colors (name, value) VALUES ('red', 1), ('green', 2), ('dark gray', 3);
-.quit
--- alire/cache/dependencies/gnatcoll_sqlite_24.0.0_4b44508a/examples/library/generate.sh
+++
@@ -1,4 +0,0 @@
-cd generated
-
-# Generate the Ada API
-gnatcoll_db2ada -api=Database -orm=ORM -dbmodel=../dbschema.txt
